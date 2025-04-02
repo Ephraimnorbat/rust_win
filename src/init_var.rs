2e82dd5cfe111e69262f7b1233b7e67f6954583d
Ephraimnorbat <ephraimnorbat@gmail.com>
2025-04-02 19:34:24 +0300

    initialzing var & mutability

---
+++ src/init_var.rs
@@ -0,0 +1,10 @@
+
+//variables must be initialized before they are used 
+//Fix the error below with least amount of modification to the code
+fn main(){
+    let x: i32=5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
